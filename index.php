c28084f630b9ec445d1f73428e6354fb64a3db57
unknown <Rone@.(none)>
2014-03-31 12:48:50 +0600

    first comit

---
+++ index.php
@@ -0,0 +1,7 @@
+<?php 
+$d=date("D");
+if($d=="Sat")echo"Have a nice day";
+else 
+echo"Today is not a saturday";	
+
+?>
---
+++ index2.php
@@ -0,0 +1,17 @@
+<?php 
+if($number>=80)
+echo"A+";
+else if($number>69 and $number<70)
+echo"A";
+else if($number>59 and $number<60)
+echo"A-";
+else if($number>49 and $number<50)
+echo"B";
+else if($number>39 and $number<40)
+echo"C";
+else if($number>32 and $number<40)
+echo"D";
+else {
+	echo"F";
+}
+?>
